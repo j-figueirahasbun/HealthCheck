eafd9f51b593d8b2e854448feb3184df33ef87d2
j-figueirahasbun <j.figueirahasbun@gmail.com>
2024-11-11 14:51:16 +0100

    Implement the ICMPHealthCheck class

---
+++ HealthCheck.Server/ICMPHealthCheck.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.NetworkInformation;
+
+namespace HealthCheck.Server
+{
+    //We implement the IhealthCheckInterface, with its CheckHealthAsync method.
+    //As this is an official .NET way to deal with health checks there is already an interface available.
+
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        //Host is set to a non-routable IP adress, which seems rather awkward.
+        //This is done for demonstration purposes so that we will be able to simulate an "unhealthy" scenario.
+        //Normally, Host and HealthyRoundtripTime variables sould be passed as parameters so that we can set them programmatically.
+        private readonly string Host = $"10.0.0.0";
+        private readonly int HealthyRoundTripTime = 300;
+       
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                //Here the ping variable has been defined using the using keyword. This declaration reduces nesting and makes more
+                //readable code. 
+                using var ping = new Ping();
+                var reply = await ping.SendPingAsync(Host);
+                switch (reply.Status)
+                {
+                    //This code handles the different scenarios we want:
+                    //Healthy, if the PING request gets a successful reply withing the round trip time of 300 ms or less
+                    //Degraded, if the PING request gets a successful reply with a round trip time greater than 300 ms
+                    //Unhealthy, if the PING request fails or an Exception is thrown
+                    case IPStatus.Success:
+                        return (reply.RoundtripTime > HealthyRoundTripTime)
+                            ? HealthCheckResult.Degraded()
+                            : HealthCheckResult.Healthy();
+                    default:
+                        return HealthCheckResult.Unhealthy();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,6 +1,15 @@
+//We use/add global to the using keyword which allows us to define some common using statements 
+//that will automatically be availabl for use within the entire project
+global using HealthCheck.Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Add HealthCheck middleware to the program:
+builder.Services.AddHealthChecks().AddCheck<ICMPHealthCheck>("ICMP");
+//The AddCheck method adds the ICMP health check to the web application pipeline, and adds it to the the HealthChecks middleware.
+//When you add something here you need to add it to the HTTP pipeline below as well
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +31,14 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"));
+//HealthCheck was added before the controllers so the controllers dont interfere with the 
+//Healthcheck and occur before those take place.
+
+//The /api/health path passed to the UseHealthChecks middleware will create a server-side
+//route for the health checks.
+//Thus this means that a new route was added to the BACKEND server to serve the healthcheck.
 
 app.MapControllers();
 
